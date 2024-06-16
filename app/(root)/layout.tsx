27ec226e61041ad763c73a2043c551c4c434fc54
dnavaria <dk17816@gmail.com>
2024-06-16 12:44:04 +0530

    initial-commit

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,10 @@
+import {ReactNode} from "react";
+
+export default function RootLayout({children,}: Readonly<{ children: ReactNode; }>) {
+    return (
+        <main>
+            SIDEBAR
+            {children}
+        </main>
+    );
+}
